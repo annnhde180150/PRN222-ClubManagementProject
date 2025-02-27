57052e245965a8748ab10b057c05d1ea19738cde
hoangphuongphan <166013675+hoangphuongphan@users.noreply.github.com>
2025-02-27 16:57:25 +0700

    Delete ClubManagementSystem_D09_Group5 directory

--- ClubManagementSystem_D09_Group5/ClubManagementSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ClubManagementSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
