015de395c622cbf13e80a678151b60823dce1dc7
hoangphuongphan <166013675+hoangphuongphan@users.noreply.github.com>
2025-03-06 20:40:02 +0700

    add Notification backend

---
+++ ClubManagementSystem_D09_Group5/Repositories/Implementation/NotificationRepository.cs
@@ -0,0 +1,53 @@
+﻿using BussinessObjects.Models;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Implementation
+{
+    public class NotificationRepository : INotificationRepository
+    {
+        private FptclubsContext _context;
+
+        public NotificationRepository()
+        {
+            if (_context == null)
+            {
+                _context = new FptclubsContext();
+            }
+        }
+
+        public async Task<Notification> AddNotificationAsync(Notification notification)
+        {
+            await _context.Notifications.AddAsync(notification);
+            await _context.SaveChangesAsync();
+            return notification;
+        }
+
+        public async Task<bool> DeleteNotificationAsync(int notificationId)
+        {
+            _context.Notifications.Remove(await GetNotificationAsync(notificationId));
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<Notification> GetNotificationAsync(int notificationId)
+        {
+            return await _context.Notifications.FirstOrDefaultAsync(noti => noti.NotificationId == notificationId)?? new Notification() {Message = "null" };
+        }
+
+        public async Task<IEnumerable<Notification>> GetNotificationsAsync(int userId)
+        {
+            return await _context.Notifications.Where(n => n.UserId == userId).ToListAsync();
+        }
+
+        public async Task<Boolean> UpdateNotificationAsync(Notification notification)
+        {
+            _context.Entry<Notification>(notification).State = EntityState.Modified;
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
