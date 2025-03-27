ff16ba807c8f6f70e7b05f3181eef4d7511a80a1
hoangphuongphan <166013675+hoangphuongphan@users.noreply.github.com>
2025-03-28 00:15:55 +0700

    adding Event Controller

---
+++ ClubManagementSystem_D09_Group5/Services/Implementation/EventService.cs
@@ -0,0 +1,41 @@
+﻿using BussinessObjects.Models;
+using Repositories.Interface;
+using Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Implementation
+{
+    public class EventService : IEventService
+    {
+        private readonly IEventRepository _eventRepository;
+
+        public EventService (IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+
+        public async Task<Event> AddEventAsync(Event Event)
+        {
+            return await _eventRepository.AddEventAsync(Event);
+        }
+
+        public async Task<Event> GetEventAsync(int EventId)
+        {
+            return await _eventRepository.GetEventAsync(EventId);
+        }
+
+        public async Task<IEnumerable<Event>> GetEventsAsync()
+        {
+            return await _eventRepository.GetEventsAsync();
+        }
+
+        public async Task<bool> UpdateEventAsync(Event Event)
+        {
+            return await _eventRepository.UpdateEventAsync(Event);
+        }
+    }
+}
---
+++ ClubManagementSystem_D09_Group5/Services/Interface/IEventService.cs
@@ -0,0 +1,17 @@
+﻿using BussinessObjects.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interface
+{
+    public interface IEventService
+    {
+        public Task<Event> AddEventAsync(Event Event);
+        public Task<bool> UpdateEventAsync(Event Event);
+        public Task<IEnumerable<Event>> GetEventsAsync();
+        public Task<Event> GetEventAsync(int EventId);
+    }
+}
