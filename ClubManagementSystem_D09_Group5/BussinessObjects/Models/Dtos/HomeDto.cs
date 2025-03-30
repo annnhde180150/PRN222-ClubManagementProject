0abc97b7d0c8180615a8ebc19837a592573751d4
Minhaza <minhazaaa1006@gmail.com>
2025-03-30 17:05:27 +0700

    Update home_view

---
+++ ClubManagementSystem_D09_Group5/BussinessObjects/Models/Dtos/HomeDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessObjects.Models.Dtos
+{
+    public class HomeDto
+    {
+        public List<PostDetailsDto> Posts { get; set; } = new();
+        public List<ClubDetailsViewDto> Clubs { get; set; } = new();
+    }
+
+}
--- ClubManagementSystem_D09_Group5/ClubManagementSystem/Controllers/HomeController.cs
+++ ClubManagementSystem_D09_Group5/ClubManagementSystem/Controllers/HomeController.cs
@@ -1,23 +1,70 @@
 using System.Diagnostics;
+using BussinessObjects.Models.Dtos;
 using ClubManagementSystem.Controllers.SignalR;
 using ClubManagementSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 using NuGet.Protocol.Plugins;
+using Services.Implementation;
+using Services.Interface;
 
 namespace ClubManagementSystem.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IPostService _postService;
+        private readonly IClubService _clubService;
+        private readonly IImageHelperService _imageHelperService;
 
-        public HomeController(ILogger<HomeController> logger, SignalRSender sender)
+        public HomeController(ILogger<HomeController> logger, SignalRSender sender, IPostService postService, IClubService clubService, IImageHelperService imageHelperService)
         {
             _logger = logger;
+            _postService = postService;
+            _clubService = clubService;
+            _imageHelperService = imageHelperService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var posts = (await _postService.GetAllPostsAsync())
+              .OrderByDescending(p => p.CreatedAt)
+              .Select(p => new PostDetailsDto
+              {
+                  PostId = p.PostId,
+                  Title = p.Title,
+                  Content = p.Content,
+                  ImageBase64 = _imageHelperService.ConvertToBase64(p.Image, "png"),
+                  CreatedAt = p.CreatedAt,
+                  Status = p.Status,
+                  LikeCount = p.Reactions.Count(r => r.IsLiked),
+                  Club = new ClubDto
+                  {
+                      ClubId = p.ClubMember.Club.ClubId,
+                      ClubName = p.ClubMember.Club.ClubName
+                  }
+              })
+              .ToList();
+
+
+            var clubs = (await _clubService.GetAllClubsAsync())
+                 .OrderByDescending(c => c.CreatedAt)
+                 .Select(c => new ClubDetailsViewDto
+                 {
+                     ClubId = c.ClubId,
+                     ClubName = c.ClubName,
+                     Description = c.Description,
+                     CreatedAt = c.CreatedAt,
+                     LogoBase64 = _imageHelperService.ConvertToBase64(c.Logo, "png")
+                 })
+                 .ToList();
+
+            var viewModel = new HomeDto
+            {
+                Posts = posts,
+                Clubs = clubs
+            };
+
+            return View(viewModel);
         }      
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
