f5c7913dd5bcc8d17c5c321966206b09795888fe
Minhaza <minhazaaa1006@gmail.com>
2025-04-01 02:03:47 +0700

    Update Post.cs

--- ClubManagementSystem_D09_Group5/BussinessObjects/Models/Post.cs
+++ ClubManagementSystem_D09_Group5/BussinessObjects/Models/Post.cs
@@ -23,7 +23,7 @@ public partial class Post
     public byte[]? Image { get; set; }
 
     [DataType(DataType.DateTime)]
-    public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? CreatedAt { get; set; } = DateTime.Now;
 
     [StringLength(50)]
     public string? Status { get; set; }
